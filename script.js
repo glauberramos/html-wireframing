ab5b590799441010e51a3e41314a3e2df306fd9f
Glauber Ramos <glauberamos@gmail.com>
2014-07-20 01:52:49 +0100

    adding button to add feedback

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
- document.addEventListener('click', function(event) {
-	var selector = unique(event.target);
-}, false);
+addingFeedback = false;
+
+$(document).ready(function() {
+	$('body').append($('<button id="add-feedback">Add feedback</button>'));
+
+	$('#add-feedback').click(function() {
+		addingFeedback = true;
+		document.addEventListener('click', addFeedback, false);
+	});
+});
+
+function addFeedback(event) {
+	if(addingFeedback && unique(event.target) != '#add-feedback') {
+		addingFeedback = false;
+		document.removeEventListener('click', addFeedback, false);
+
+		var selector = unique(event.target);
+		var nextFeedbackId = $('.feedback').length + 1;
+
+		var feedback = new Feedback();
+		feedback.set('feedbackId', nextFeedbackId);
+		feedback.set('selector', selector);
+		feedback.save({
+		  success: function(comments) {
+		  	var feedback = new uifeedback.model.feedback(nextFeedbackId, selector); 
+		  	var feedbackHtml = Mustache.render(feedbackTemplate, feedback);
+			$(selector).prepend(feedbackHtml);
+
+			$('[data-id=' + nextFeedbackId + ']').find('.btn').unbind('click').bind('click', function() {
+	    		addComment(nextFeedbackId);
+	    	});
+		  }
+		});
+	}
+}
